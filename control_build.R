73fa42fcb4076ac3f809e90f7a48cefcdcf86c2f
Julien Brun <brun.julien@gmail.com>
2021-04-14 09:33:39 -0700

    moving functions to their own script

--- control_build.R
+++ control_build.R
@@ -1,40 +1,8 @@
 library(tidyverse)
 
+source("hysplit_batch_functions.R")
 
-#' Create the CONTROL file for the HYSPLIT model
-#'
-#' @param out_file
-#' @param date
-#' @param locations
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#'
-create_control <- function(out_file, date, locations, dir_templates="file_templates/") {
 
-  # Build paths
-  control_file <- "CONTROL_template"
-  control_path <- file.path(dir_templates, control_file)
-  out_full <- file.path("output", out_file)
-  out_conn <- file(out_full) # how to create file in R that you write line-by-line
-
-  # Read control file in
-  control_lines <- readLines(control_path) # reading in template
-
-  # Create the parameters
-  nb_fire <- nrow(locations)
-
-  writeLines(c(date, nb_fire), out_conn) # how you write lines for vectors. Date as one line, number fire as one line, written to out_conn file
-  close(out_conn) # closing the file
-
-  # Add lat long to file
-  write.table(locations, out_full, row.names = FALSE, col.names = FALSE, append=TRUE) # adding a table
-
-  # Add template
-  write(control_lines, out_full, append = TRUE) # Writing content of template is different because it's a block of text
-}
 
 # create folder name
 folder_name <- "July"

40eab204e40f0a7c75c01dddccd39fa11dc28303
Maggie Klope <klope@aurora.nceas.ucsb.edu>
2021-05-04 15:43:19 -0700

    updates to change file extension & splitting up the control template

--- control_function_troubleshooting.R
+++ control_function_troubleshooting.R
@@ -1,6 +1,5 @@
-create_control <- function(out_file, date, locations, arl_file, arl_dir, dir_templates = "file_templates/", dir_save = "July") {
+create_control <- function(out_file, date, locations, arl_file, arl_dir, dir_templates = "file_templates/", extension, dir_save = "July") {
 
-  for (extension in c(".001", ".002", ".003", ".004", ".005")){
   # Build paths
   control_file <- "CONTROL_template"
   control_path <- file.path(dir_templates, control_file)
@@ -24,11 +23,11 @@ create_control <- function(out_file, date, locations, arl_file, arl_dir, dir_tem
 
   # Add template
   write(control_lines, out_full, append = TRUE) # Writing content of template is different because it's a block of text
-  }
+
 }
 
 # Call the function
-create_control(filename, july_date, my_locations, arl_file = arl, arl_dir = file_path_arl)
+create_control(filename, july_date, my_locations, arl_file = arl, arl_dir = file_path_arl, extension = ".001")
 
 # create file name
 filename <- "CONTROL_july"
@@ -52,5 +51,39 @@ july_date <- as.character(july_date[1])
 
 arl <- "JunJuly.ARL"
 file_path_arl <- "c:\\hysplit4\\met\\2019\\Jun_July\\"
+
+# changing the cdump
+# Build paths
+control_file_1 <- "CONTROL_template_1"
+control_file_2 <- "CONTROL_template_2"
+control_path_1 <- file.path("file_templates/", control_file_1)
+control_path_2 <- file.path("file_templates/", control_file_2)
+out_full <- file.path("July", paste0(filename, ".001")) # saves to the file path you choose (currently set to July)
+out_conn <- file(out_full) # how to create file in R that you write line-by-line
+
+# Read control file in
+control_lines_1 <- readLines(control_path_1) # reading in template
+control_lines_2 <- readLines(control_path_2)
+
+# Create the parameters
+nb_fire <- nrow(my_locations)
+
+writeLines(c(july_date, nb_fire), out_conn) # how you write lines for vectors. Date as one line, number fire as one line, written to out_conn file
+close(out_conn) # closing the file
+
+# Add lat long to file
+write.table(my_locations, out_full, row.names = FALSE, col.names = FALSE, append=TRUE) # adding a table
+
+# adding ARl file & file path information
+write(c("0", "10000.0 ", "1", file_path_arl, arl), out_full, append = TRUE)
+
+# Add template part 1
+write(control_lines_1, out_full, append = TRUE) # Writing content of template is different because it's a block of text
+
+# adding cdump with extension
+write(paste0("cdump", ".", "001"), out_full, append = TRUE)
+
+# adding template part 2
+write(control_lines_2, out_full, append = TRUE)
 
 

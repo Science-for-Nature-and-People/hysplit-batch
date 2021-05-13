59eb582da8d2857ba9e054db07650eec6c592aea
Maggie Klope <klope@aurora.nceas.ucsb.edu>
2021-05-13 14:49:08 -0700

    updating file paths

--- batch_config_files.R
+++ batch_config_files.R
@@ -16,7 +16,7 @@ emitimes_file <- "/home/shares/snapp-wildfire/HYSPLIT_samplefiles/EMITIMES_july"
 fake_dates <- seq(ymd('2012-01-07'),ymd('2012-12-22'), by = '1 month') %>% month() %>% paste0("run_", .) %>% file.path(getwd(),.)
 
 # folder to save model files
-# dir.create("model_files")
+dir.create("model_files")
 
 # # create extension list
 # extensions <- seq(0, (length(fake_dates)/1000), by = 0.001)
@@ -25,12 +25,12 @@ foreach(model_run = fake_dates) %dopar% {
   source(file.path(getwd(),"hysplit_batch_functions.R"))
 
   # copy the EMITIMES files
-  emitimes_run <- file.path("/home/klope/","EMITIMES")
+  emitimes_run <- file.path("model_files/","EMITIMES")
   # file.copy(emitimes_file, emitimes_run, overwrite = TRUE)
-  file.copy(emitimes_file, file.path("/home/klope/", "EMITIMES"), overwrite = TRUE)
+  file.copy(emitimes_file, file.path("model_files/", "EMITIMES"), overwrite = TRUE)
 
   # copy SETUP
-  create_setup <- create_setup("/home/klope/", extension = "")
+  create_setup <- create_setup("model_files/", extension = "")
 
   # Get information from EMITIMES
   control_info <- read_emitimes(emitimes_run)
@@ -38,7 +38,7 @@ foreach(model_run = fake_dates) %dopar% {
     dplyr::select(LAT, LON)
 
   # Create the CONTROL file
-  control_filename <- file.path("/home/klope/", "CONTROL")
+  control_filename <- file.path("model_files/", "CONTROL")
   create_control(control_filename, control_info$date, control_locations, control_info$runtime, "July.ARL", "/home/klope/hysplit/5.0.0/working/", extension = "")
 
   #### Run the model #####

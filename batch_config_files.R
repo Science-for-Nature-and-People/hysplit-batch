c25ce23ca8e57e1305089d9dfc3423e512dec2a5
Julien Brun <brun@nceas.ucsb.edu>
2021-06-02 10:39:42 -0700

    better path and file handling

--- batch_config_files.R
+++ batch_config_files.R
@@ -1,44 +1,49 @@
+#' This script loops through a series of runs of the HYSPLIT model
+
+## Libraries
 library(tidyverse)
 library(lubridate)
 library(foreach)
 
 
-
+## Paths and files
+shared_dir <- "/home/shares/snapp-wildfire/HYSPLIT_samplefiles/"
+run_dir <- "~/run2"
 # folder to save model files in your home directory
-dir.create("~/run", showWarnings = FALSE)
+dir.create(run_dir, showWarnings = FALSE)
 
-# set working directory
-# setwd("~/run")
+# Move the files you need to run the model
+file.copy(file.path(shared_dir,"ASCDATA.CFG"), run_dir, overwrite = TRUE)
+file.copy(list.files(path=shared_dir, pattern=".ARL", full.names = TRUE), run_dir, overwrite = FALSE) # To be changed as we do not want to move those files around
 
-# start cluster
+
+## Start cluster
 nb_cores <- 12  # Aurora has 96 cores
 cl <- parallel::makeCluster(nb_cores)
 doParallel::registerDoParallel(cl)
 
+
+## Prepare the list of dates to iterate through
 # EMITIMES file
-emitimes_file <- "/home/shares/snapp-wildfire/HYSPLIT_samplefiles/EMITIMES_july"
+emitimes_file <- file.path(shared_dir,"EMITIMES_july")
 
 # For the example using the same EMITIMES
-fake_dates <- seq(ymd('2012-01-07'),ymd('2012-12-22'), by = '1 month') #%>%
-  # month() %>%
-  # paste0("run_", .) %>%
-  # file.path(getwd(),.)
+fake_dates <- seq(ymd('2012-01-07'), ymd('2012-12-22'), by = '1 month') #%>%
 
-# # create extension list
-# extensions <- seq(0, (length(fake_dates)/1000), by = 0.001)
 
+## MAIN loop
 foreach(model_run = fake_dates) %dopar% {
   # setwd("/home/klope/R/hysplit-batch")
   # source(file.path("/home/klope/R/hysplit-batch","hysplit_batch_functions.R"))
   source(file.path(getwd(),"hysplit_batch_functions.R"))
 
   # copy the EMITIMES files
-  emitimes_run <- file.path("~/run", paste0("EMITIMES", ".", model_run))
+  emitimes_run <- file.path(run_dir, paste0("EMITIMES", ".", model_run))
   # file.copy(emitimes_file, emitimes_run, overwrite = TRUE)
   file.copy(emitimes_file, emitimes_run, overwrite = TRUE)
 
   # copy SETUP
-  create_setup <- create_setup("~/run", extension = model_run)
+  create_setup <- create_setup(run_dir, extension = model_run)
 
   # Get information from EMITIMES
   control_info <- read_emitimes(emitimes_run)
@@ -46,12 +51,12 @@ foreach(model_run = fake_dates) %dopar% {
     dplyr::select(LAT, LON)
 
   # Create the CONTROL file
-  control_filename <- file.path("~/run", "CONTROL")
+  control_filename <- file.path(run_dir, "CONTROL")
   create_control(control_filename, control_info$date, control_locations, control_info$runtime, "July.ARL", "./", extension = model_run)
 
   #### Run the model #####
   repo <- getwd()
-  setwd("~/run")
+  setwd(run_dir)
   system(sprintf("../hysplit/5.0.0/exec/hycs_std %s", model_run))
   setwd(repo)
 
